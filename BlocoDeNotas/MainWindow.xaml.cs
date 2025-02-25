d55edee506762f5efbc1b970c441ed3c899dca77
gustavofns <116985530+gustavofns@users.noreply.github.com>
2025-02-25 08:15:08 -0300

    * Atualização de requisitos: - Requer Windows 11 22H2 ou superior

--- BlocoDeNotas/MainWindow.xaml.cs
+++ BlocoDeNotas/MainWindow.xaml.cs
@@ -24,13 +24,16 @@ namespace BlocoDeNotas
             DragMove();
         }
 
-        // Verifica a versão do sistema operacional
+        /* 
+         * Verifica a versão do sistema operacional:
+         * - Removido o suporte para Windows 11 21H2
+         */
         public void VerificarOS()
         {
-            if (!(Environment.OSVersion.Version.Build >= 22000))
+            if (!(Environment.OSVersion.Version.Build >= 22621))
             {
                 MessageBox.Show("Este aplicativo não é compatível com esta versão do Windows.\n" +
-                    "Por favor atualize para Windows 11 21H2 ou superior.",
+                    "Por favor atualize para Windows 11 22H2 ou superior.",
                     "Bloco de notas", MessageBoxButton.OK, MessageBoxImage.Error);
                 Application.Current.Shutdown();
                 Close();
